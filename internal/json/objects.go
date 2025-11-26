24ef74d44fba2fa7402226ea5ff2dac2f6628492
agialias <agialias4000@gmail.com>
2025-11-26 13:25:28 +0000

    MVP successfully GETs CAP information using device flow OAUTH.

---
+++ internal/json/objects.go
@@ -0,0 +1,74 @@
+package json
+
+import "time"
+
+type ConditionalAccessPolicy struct {
+	OdataContext       string    `json:"@odata.context"`
+	MicrosoftGraphTips string    `json:"@microsoft.graph.tips"`
+	ID                 string    `json:"id"`
+	TemplateID         any       `json:"templateId"`
+	DisplayName        string    `json:"displayName"`
+	CreatedDateTime    time.Time `json:"createdDateTime"`
+	ModifiedDateTime   time.Time `json:"modifiedDateTime"`
+	State              string    `json:"state"`
+	Conditions         struct {
+		UserRiskLevels             []string `json:"userRiskLevels"`
+		SignInRiskLevels           []any    `json:"signInRiskLevels"`
+		ClientAppTypes             []string `json:"clientAppTypes"`
+		ServicePrincipalRiskLevels []any    `json:"servicePrincipalRiskLevels"`
+		InsiderRiskLevels          any      `json:"insiderRiskLevels"`
+		Platforms                  any      `json:"platforms"`
+		Locations                  any      `json:"locations"`
+		Devices                    any      `json:"devices"`
+		ClientApplications         any      `json:"clientApplications"`
+		Applications               struct {
+			IncludeApplications                         []string `json:"includeApplications"`
+			ExcludeApplications                         []any    `json:"excludeApplications"`
+			IncludeUserActions                          []any    `json:"includeUserActions"`
+			IncludeAuthenticationContextClassReferences []any    `json:"includeAuthenticationContextClassReferences"`
+			ApplicationFilter                           any      `json:"applicationFilter"`
+		} `json:"applications"`
+		Users struct {
+			IncludeUsers                 []string `json:"includeUsers"`
+			ExcludeUsers                 []any    `json:"excludeUsers"`
+			IncludeGroups                []any    `json:"includeGroups"`
+			ExcludeGroups                []string `json:"excludeGroups"`
+			IncludeRoles                 []any    `json:"includeRoles"`
+			ExcludeRoles                 []any    `json:"excludeRoles"`
+			IncludeGuestsOrExternalUsers any      `json:"includeGuestsOrExternalUsers"`
+			ExcludeGuestsOrExternalUsers any      `json:"excludeGuestsOrExternalUsers"`
+		} `json:"users"`
+	} `json:"conditions"`
+	GrantControls struct {
+		Operator                           string   `json:"operator"`
+		BuiltInControls                    []string `json:"builtInControls"`
+		CustomAuthenticationFactors        []any    `json:"customAuthenticationFactors"`
+		TermsOfUse                         []any    `json:"termsOfUse"`
+		AuthenticationStrengthOdataContext string   `json:"authenticationStrength@odata.context"`
+		AuthenticationStrength             struct {
+			ID                                    string    `json:"id"`
+			CreatedDateTime                       time.Time `json:"createdDateTime"`
+			ModifiedDateTime                      time.Time `json:"modifiedDateTime"`
+			DisplayName                           string    `json:"displayName"`
+			Description                           string    `json:"description"`
+			PolicyType                            string    `json:"policyType"`
+			RequirementsSatisfied                 string    `json:"requirementsSatisfied"`
+			AllowedCombinations                   []string  `json:"allowedCombinations"`
+			CombinationConfigurationsOdataContext string    `json:"combinationConfigurations@odata.context"`
+			CombinationConfigurations             []any     `json:"combinationConfigurations"`
+		} `json:"authenticationStrength"`
+	} `json:"grantControls"`
+	SessionControls struct {
+		DisableResilienceDefaults       any `json:"disableResilienceDefaults"`
+		ApplicationEnforcedRestrictions any `json:"applicationEnforcedRestrictions"`
+		CloudAppSecurity                any `json:"cloudAppSecurity"`
+		PersistentBrowser               any `json:"persistentBrowser"`
+		SignInFrequency                 struct {
+			Value              any    `json:"value"`
+			Type               any    `json:"type"`
+			AuthenticationType string `json:"authenticationType"`
+			FrequencyInterval  string `json:"frequencyInterval"`
+			IsEnabled          bool   `json:"isEnabled"`
+		} `json:"signInFrequency"`
+	} `json:"sessionControls"`
+}
--- main.go
+++ main.go
@@ -2,8 +2,60 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+
+	"github.com/agialias-dev/capmate/internal/graph"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Go Graph Tutorial")
+	fmt.Println()
+
+	// Load .env files
+	// .env.local takes precedence (if present)
+	godotenv.Load(".env.local")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	graphHelper := graph.NewGraphHelper()
+
+	graph.InitializeGraph(graphHelper)
+
+	graph.GreetUser(graphHelper)
+
+	var choice int64 = -1
+
+	for {
+		fmt.Println("Please choose one of the following options:")
+		fmt.Println("0. Exit")
+		fmt.Println("1. Make a Graph call")
+		fmt.Println("2. Display Access Token")
+
+		_, err = fmt.Scanf("%d", &choice)
+		if err != nil {
+			choice = -1
+		}
+
+		switch choice {
+		case 0:
+			// Exit the program
+			fmt.Println("Goodbye...")
+		case 1:
+			// Make a Graph call
+			graph.MakeGraphCall(graphHelper)
+		case 2:
+			// Display Access Token
+			graph.DisplayAccessToken(graphHelper)
+		default:
+			fmt.Println("Invalid choice! Please try again.")
+		}
+
+		if choice == 0 {
+			break
+		}
+	}
 }
